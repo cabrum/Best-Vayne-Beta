b35408f044852e2b15cc43d5ebfefc63a9332405
cabrum <drigo@usa.com>
2017-04-23 22:17:21 -0300

    Add files via upload

---
+++ Best Vayne/Best Vayne/Utility/Evade.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using SharpDX;
+
+namespace Best_Vayne.Utility
+{
+    /// <summary>
+    /// Evade assistance
+    /// </summary>
+    public class Evade
+    {
+    }
+}
